d992d57ef9c6cfd5dd3e63520d04b106a4800063
Lucas Alves <lucas.d.alves9@aluno.senai.br>
2023-12-13 21:38:25 -0300

    foi

---
+++ games-api/js/main.js
@@ -0,0 +1,57 @@
+import {getAllGames, createGame, updateGame, deleteGame } from "./service.js";
+
+window.onload = () => {
+    loadGames();
+};
+ 
+const loadGames = () => {
+    const dataContainer = 
+        document.getElementById('data-container');
+        getAllGames().then(resp =>{
+        resp.forEach((jogo) => {
+            const gamesElement = 
+                document.createElement('div');
+                gamesElement.innerHTML = 
+`
+<div class="elemento">
+<img class="cardimg" src="${jogo.img}" alt="${jogo.nome}">
+<hr>
+<h5 class"cardtitle"> ${jogo.nome} </h5>
+<hr>
+<p class="cardText"> R$ ${jogo.preco}</p>
+</div>
+`;
+             dataContainer.appendChild(gamesElement);
+        });
+    })
+};
+
+document.getElementById ('btnCreate') .addEventListener('click', () => {
+    const jogo = {
+        "nome": "The legend of Zelda",
+        "img": "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
+        "preco": 100,
+    };
+    createGame (jogo);
+});
+
+document.getElementById ('btnDelete') .addEventListener ('click', ()=> {
+    const jogo = {
+        "nome": "The legend of Zelda 2",
+        "img": "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
+        "preco": 100,
+        "id": 3
+    };
+    deleteGame (jogo);
+});
+
+document.getElementById ('btnUpdate') .addEventListener ('click', () => {
+    const jogo = {
+        nome: "The legend of Zelda o retorno",
+        img: "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
+        preco: 3000,
+        id: 3
+    };
+    updateGame(jogo)
+});
+
